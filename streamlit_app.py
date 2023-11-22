1e5d61adcab75e6d92a5e389790b3114825e04a9
hmonr0i <151762415+hmonr0i@users.noreply.github.com>
2023-11-22 09:43:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-streamlit.header('My Mom's New Healthy Diner')
-
+streamlit.title('My Mom's New Healthy Diner')
+streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
 streamlit.text('🥣  Omega 3 y avena con arándanos')
 streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
 streamlit.text('🐔 Huevo duro de gallinas camperas')
 streamlit.text('🥑🍞 Totada de Aguacate')
-streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
+
