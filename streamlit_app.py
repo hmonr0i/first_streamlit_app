bde4bfaf7ae5830316cdf60f17b2ed9801680a4b
hmonr0i <151762415+hmonr0i@users.noreply.github.com>
2023-11-22 09:50:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('🥑🍞 Totada de Aguacate')
 
 streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
 
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
